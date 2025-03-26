2d980560a22adc89a7dc7804b55b2343d63b84d5
gou <19513317149@163.com>
2025-03-26 09:58:35 +0800

    【代码随想录】提交basic

---
+++ Source/Array/include/basic.hh
@@ -0,0 +1,19 @@
+#ifndef BASIC_HH
+#define BASIC_HH
+
+/*
+参考网页地址：https://programmercarl.com/数组理论基础.html
+time：2025-3-26
+*/
+#include <iostream>
+
+void basic();
+// 数组的元素是不能删的，只能覆盖。
+
+// 这里记录理论知识
+// 数组是存放在连续内存空间上的相同类型数据的集合。
+// 数组下标都是从0开始的。
+// 数组内存空间的地址是连续的
+// 如果使用C++的话，要注意vector 和 array的区别，vector的底层实现是array，严格来讲vector是容器，不是数组。
+// 数组的元素是不能删的，只能覆盖。
+#endif
--- Source/Array/source/main.cpp
+++ Source/Array/source/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "log.hh"
-
+#include "basic.hh"
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 设置日志等级
     LOG log;
     log.SetLevel(log.LOG_LEVEL_Info); // 写不写都一样，默认是Info
+    // 这里是basic
+    basic();
 }
