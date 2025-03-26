a83ae89ca8eaabd4710402f842ee00809522d299
gou <19513317149@163.com>
2025-03-26 15:03:58 +0800

    【代码随想录】提交暴力插入法

--- Source/Array/source/binary_search.cpp
+++ Source/Array/source/binary_search.cpp
@@ -61,3 +61,55 @@ int Solution_binary::ajar_search(std::vector<int> &nums, int target)
     // 未找到目标值
     return -1;
 }
+int Solution_binary::searchInsert(std::vector<int> &nums, int target) // 暴力插入
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[0] > target) // 目标值在数组所有元素之前
+        {
+            nums.insert(nums.begin(), target);
+            std::cout << "after insert" << std::endl; // 打印出来插入后的数组
+            for (int i = 0; i < nums.size(); i++)
+            {
+                std::cout << nums[i] << std::endl;
+            }
+            return 0;
+        }
+        else if (nums[i] == target) // 目标值在数组中等于某个元素
+        {
+            nums.insert(nums.begin() + i, target);
+            std::cout << "after insert" << std::endl; // 打印出来插入后的数组
+            for (int i = 0; i < nums.size(); i++)
+            {
+                std::cout << nums[i] << std::endl;
+            }
+            return i;
+        }
+        else if (nums[i] > target) // 目标值在数组中大于某个元素
+        {
+            nums.insert(nums.begin() + i, target);
+            std::cout << "after insert" << std::endl; // 打印出来插入后的数组
+            for (int i = 0; i < nums.size(); i++)
+            {
+                std::cout << nums[i] << std::endl;
+            }
+            return i;
+        }
+        else if (nums.size() < target) // 目标值在数组所有元素之后
+        {
+            nums.insert(nums.end(), target);
+            std::cout << "after insert" << std::endl; // 打印出来插入后的数组
+            for (int i = 0; i < nums.size(); i++)
+            {
+                std::cout << nums[i] << std::endl;
+            }
+            return nums.size() - 1;
+        }
+    }
+    return -1;
+}
+int Solution_binary::searchInsert2(std::vector<int> &nums, int target) // 二分法
+{
+}
--- Source/Array/source/main.cpp
+++ Source/Array/source/main.cpp
@@ -8,5 +8,20 @@ int main()
     LOG log;
     log.SetLevel(log.LOG_LEVEL_Info); // 写不写都一样，默认是Info
 
+    /*
+    这里是basic
+    basic();
+    */
+
+    // 这里是是二分查找
+    // bin_search();
+    Solution_binary solution_binary;
+    std::vector<int> nums = {0, 2, 3, 4, 5, 6, 7, 8};
+    int loc = solution_binary.close_search(nums, 5);
+    std::cout << loc << std::endl;
+    int loc2 = solution_binary.ajar_search(nums, 10);
+    std::cout << loc2 << std::endl;
+    int loc3 = solution_binary.searchInsert(nums, 1);
+    std::cout << "insert loc:" << loc3 << std::endl;
     return 0;
 }
